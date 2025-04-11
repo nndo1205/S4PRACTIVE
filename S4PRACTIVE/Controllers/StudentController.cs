6a2409eae7afd11985a18d31520af18b037f1017
ASUS <ASUS@LAPTOP-B5IMLAC3>
2025-04-12 00:07:47 +0700

    Add project files.

---
+++ S4PRACTIVE/Controllers/StudentController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Example.Controllers
+{
+    public class StudentController : Controller
+    {
+        [Route("HomeStudent")]
+        [Route("Student")]
+        [Route("Student/ListAll")]
+        [Route("Liet-ke-danh-sach-sinh-vien")]
+        public string ListAll()
+        {
+            return "Liệt kê danh sách sinh viên";
+        }
+        public ContentResult Index()
+        {
+            return new ContentResult()
+            {
+                Content = "Welcome to Student Page",
+             ContentType = "text/plain"
+            };
+        }
+        [Route("File/Download-file")]
+        public FileResult  Index()
+        {   
+            return File("/ling.pdf","application/pdf");
+        }
+        public string ListOnlyOne()
+        {
+            return "Liệt kê một sinh viên có id cụ thể";
+        }
+
+        public string EditStudent()
+        {
+            return "Chỉnh sửa thông tin một sinh viên có id cụ thể";
+        }
+
+        public string AddStudent()
+        {
+            return "Thêm thông tin một sinh viên";
+        }
+
+        public string DelStudent()
+        {
+            return "Xóa thông tin một sinh viên";
+        }
+    }
+}
